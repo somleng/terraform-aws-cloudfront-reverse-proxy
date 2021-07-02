096fb8a523730472b32878532bb14f4b696264b2
Samnang Chhun <samnang.chhun@gmail.com>
2021-07-02 18:56:46 +0700

    Update outputs

---
+++ outputs.tf
@@ -0,0 +1,20 @@
+output "acm_certificate_arn" {
+  description = "The ARN of the certificate"
+  value       = aws_acm_certificate.this.arn
+}
+
+output "cloudfront_distribution_id" {
+  description = "The identifier for the distribution."
+  value       = aws_cloudfront_distribution.this.id
+}
+
+output "cloudfront_distribution_arn" {
+  description = "The ARN (Amazon Resource Name) for the distribution."
+  value       = aws_cloudfront_distribution.this.arn
+}
+
+output "fqdn" {
+  description = "FQDN built using the zone domain and name."
+  value       = aws_route53_record.this.fqdn
+}
+
